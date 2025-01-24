6747dd63138abd96367c0fb04afd4d16f795480f
Vladimir <f7beu@yandex.ru>
2025-01-24 17:34:44 +0300

    первая версия

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,26 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const card = document.querySelector("#card-template").content;
+const placesList = document.querySelector(".places__list");
+
+function workingCard() {
+  initialCards.forEach((el) => {
+    const cardTitle = el.name;
+    const cardImage = el.link;
+    const cardItem = card.querySelector(".places__item").cloneNode(true);
+    cardItem.querySelector(".card__image").src = cardImage;
+    cardItem.querySelector(".card__title").textContent = cardTitle;
+    placesList.append(cardItem);
+    const deleteButton = document.querySelector(".card__delete-button");
+    deleteButton.addEventListener("click", deleteCard);
+  });
+}
+workingCard(initialCards);
+
+function deleteCard() {
+  const deleteButton = document.querySelector(".card__delete-button");
+  const listItem = deleteButton.closest(".places__item");
+  listItem.remove();
+}
