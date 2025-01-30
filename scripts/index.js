38790fde2e2726e6d25d7737f9d298d9b35a8a2e
Vladimir <f7beu@yandex.ru>
2025-01-30 12:03:05 +0300

    третья версия

--- scripts/index.js
+++ scripts/index.js
@@ -12,17 +12,7 @@ const card = document.querySelector("#card-template").content;
 
 const placesList = document.querySelector(".places__list");
 
-function addCard(arr) {
-  arr.forEach((el) => {
-    const cardTitle = el.name;
-    const cardImage = el.link;
-
-    placesList.append(createCard(cardTitle, cardImage));
-  });
-}
-addCard(initialCards);
-
-function createCard(cardTitle, cardImage) {
+function createCard(cardTitle, cardImage, deleteCard) {
   const cardElement = card.querySelector(".card").cloneNode(true);
   const deleteButton = cardElement.querySelector(".card__delete-button");
 
@@ -30,10 +20,19 @@ function createCard(cardTitle, cardImage) {
   cardElement.querySelector(".card__image").alt = cardTitle;
   cardElement.querySelector(".card__title").textContent = cardTitle;
 
-  deleteButton.addEventListener("click", function () {
-    const listItem = deleteButton.closest(".places__item");
-    listItem.remove();
-  });
+  deleteButton.addEventListener("click", deleteCard);
 
   return cardElement;
 }
+
+function deleteCard(event) {
+  const deleteItem = event.target.closest(".places__item");
+  deleteItem.remove();
+}
+
+initialCards.forEach((el) => {
+  const cardTitle = el.name;
+  const cardImage = el.link;
+
+  placesList.append(createCard(cardTitle, cardImage, deleteCard));
+});
